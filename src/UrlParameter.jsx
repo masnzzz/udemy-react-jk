72eba4076d7770a09b7d97d7c3e9071610690c5e
masnzzz <z3heng0326@gmail.com>
2022-07-19 17:16:51 +0900

    router/クエリパラメータを使用する

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,11 +1,15 @@
-import { useParams } from "react-router-dom"
+import { useLocation, useParams } from "react-router-dom"
 
 export const UrlParameter = () => {
     const { id } = useParams();
+    const { search } = useLocation();
+    const query = new URLSearchParams(search);
+    console.log(query);
     return (
         <div>
             <h1>UrlParameter</h1>
             <p>Parameter is {id}</p>
+            <p>Query Parameter is {query.get("name")}</p>
         </div>
     )
 }
