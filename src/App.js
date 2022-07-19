99b6a8b31d5311eb0c7ccc8042a09fcb04da3682
masnzzz <z3heng0326@gmail.com>
2022-07-19 15:22:22 +0900

    router/ BrowserRouterを利用したルーティング
    exactをつけるとパスの完全一致

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom"
+
+import { Home } from "./Home"
+import { Page1 } from "./Page1"
+import { Page2 } from "./Page2"
+
 export const App = () => {
 
   return (
-    <>
-      <p>react</p>
-    </>
-  )
-}
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+        <br />
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
