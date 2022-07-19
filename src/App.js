c338b8516173755ce541d12fa6d5a5860da9d831
masnzzz <z3heng0326@gmail.com>
2022-07-19 11:09:00 +0900

    準備/レンダリングが発生するコードを実装
    state, props が変更されたときに配列内の2000件読み込みが実行される

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
  import { useState } from "react";
+import { ChildArea } from "./ChildArea";
 
 export const App = () => {
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
 
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  }
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
 
   return (
-    <>
-      <p>{count}</p>
-      <button onClick={ onClickCountUp}>Count Up</button>
-    </>
+    <div className="App">
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
+    </div>
   )
 }
