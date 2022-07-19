c55163655b9dfd4efa1d63649dd71a9c153ff9f2
masnzzz <z3heng0326@gmail.com>
2022-07-19 12:51:53 +0900

    style/CSS Moduleを利用
    コンポーネントに対応するCSSファイルを用意するやり方

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { CssModules } from "./components/CssModules";
 import { InlineStyle } from "./components/InlineStyle";
 
 export const App = () => {
   return (
     <div className="App">
       <InlineStyle />
+      <CssModules />
     </div>
   )
 }
