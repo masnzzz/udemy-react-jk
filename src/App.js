de1d727bfe306b999882ce870197372c3ece9565
masnzzz <z3heng0326@gmail.com>
2022-07-19 15:59:27 +0900

    router/ルート定義を分割

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom"
+import { BrowserRouter, Link } from "react-router-dom"
 
-import { Home } from "./Home"
-import { Page1 } from "./Page1"
-import { Page1DetailA } from "./Page1DetailA";
-import { Page1DetailB } from "./Page1DetailB";
-import { Page2 } from "./Page2"
-
+import { Router } from "./router/Router";
 export const App = () => {
 
   return (
@@ -17,31 +12,8 @@ export const App = () => {
         <br />
         <Link to="/page2">Page2</Link>
         <br />
+        <Router />
       </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-
-        <Route path="/page1" render={({ match: { url } }) => (
-          <Switch>
-            <Route exact path={url}>
-              <Page1 />
-            </Route>
-            <Route path={`${url}/detailA`}>
-              <Page1DetailA />
-            </Route>
-            <Route path={`${url}/detailB`}>
-              <Page1DetailB />
-            </Route>
-          </Switch>
-        )}>
-
-        </Route>
-        <Route path="/page2">
-          <Page2 />
-        </Route>
-      </Switch>
     </BrowserRouter>
   );
 };
