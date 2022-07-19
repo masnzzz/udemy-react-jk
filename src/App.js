049a6f3ef65dbc55fc6ca6840b111edb070def06
masnzzz <z3heng0326@gmail.com>
2022-07-19 15:03:08 +0900

    delete files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
- import { useState } from "react";
-
 export const App = () => {
-  const [count, setCount] = useState(0);
-
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  }
 
   return (
     <>
-      <p>{count}</p>
-      <button onClick={ onClickCountUp}>Count Up</button>
+      <p>react</p>
     </>
   )
 }
